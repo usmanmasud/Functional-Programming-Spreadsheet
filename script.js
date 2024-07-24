0ef0cae53deddd46c185bb56f32d78b75d958c53
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-07-25 00:41:07 +0100

    uodate script

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-const isEven = (num) => {
-    return num %2 == 0 ? true : false
+const isEven = num => num % 2 === 0;
+const sum = nums => nums.reduce((acc, el) => acc + el, 0);
+const average = nums => sum(nums) / nums.length;
+
+const median = nums => {
+  const sorted = nums.slice().sort((a, b) => a - b);
+  const length = sorted.length;
+  const middle = length / 2 - 1;  
+ return isEven(length) ? average([sorted[middle], sorted[middle +1]]) : sorted[Math.ceil(middle)];
+} 
+
+const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
+const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
+
+window.onload = () => {
+  const container = document.getElementById("container");
+  const createLabel = (name) => {
+    const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
   }
-  const sum = nums => nums.reduce((acc, el) => acc + el, 0);
-  
-  const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
-  const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
-  
-  window.onload = () => {
-    const container = document.getElementById("container");
-    const createLabel = (name) => {
-      const label = document.createElement("div");
-      label.className = "label";
-      label.textContent = name;
-      container.appendChild(label);
-    }
-    const letters = charRange("A", "J");
-    letters.forEach(createLabel);
-    range(1, 99).forEach(number => {
-      createLabel(number);
-      letters.forEach(letter => {
-        const input = document.createElement("input");
-        input.type = "text";
-        input.id = letter + number;
-        input.ariaLabel = letter + number;
-        container.appendChild(input);
-      })
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach(number => {
+    createLabel(number);
+    letters.forEach(letter => {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = letter + number;
+      container.appendChild(input);
     })
-  }
+  })
+}
